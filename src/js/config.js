d74c9cfd07e8c6e7d940384012e1887f3b1b3761
Kyukyoung Song (James Song) <skdmlrh12@gmail.com>
2024-08-02 10:44:11 +0900

    feat: messenger with vanillaJS and firebase

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAl_9jIqT5_AvSF3zLnvzV2AbTp4kYjJMY",
+  authDomain: "ssongq-messenger.firebaseapp.com",
+  projectId: "ssongq-messenger",
+  storageBucket: "ssongq-messenger.appspot.com",
+  messagingSenderId: "1092077826937",
+  appId: "1:1092077826937:web:c3137437ba3f61adcbb5a8",
+  databaseURL: "https://ssongq-messenger-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
